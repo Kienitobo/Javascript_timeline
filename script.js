92d55c038ad5e4ab3995550e94fc785df86a9898
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2022-11-22 21:35:02 +0100

    new files added
    added html, css, script files

---
+++ script.js
@@ -0,0 +1,21 @@
+let pageTop;
+const containers = document.getElementsByClassName("container");
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  pageTop = window.pageYOffset / 1.5;
+  for (let i = 0; i < containers.length; i++) {
+    let containerTop = containers[i].getBoundingClientRect().top;
+
+    if (containerTop < pageTop) {
+      containers[i].classList.add("show");
+    } else {
+      containers[i].classList.remove("show");
+    }
+  }
+}
+
+$(".subcontent").hide();
+$(".content").click(function () {
+  $(".subcontent").toggle();
+});
