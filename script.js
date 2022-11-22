8952209a3ccbb40fee477484f728d5759b88ad1b
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2022-11-22 21:56:56 +0100

    Update script.js
    changed pageTop

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const containers = document.getElementsByClassName("container");
 window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
-  pageTop = window.pageYOffset / 1.5;
+  pageTop = window.pageYOffset / 1.2;
   for (let i = 0; i < containers.length; i++) {
     let containerTop = containers[i].getBoundingClientRect().top;
 
